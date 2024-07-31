eb26010b162d401b43f7c44cb55c1bd0af856e76
winetree94 <winetree94@gmail.com>
2024-08-01 01:17:33 +0900

    feat: remove starlight

--- packages/playground/astro.config.mjs
+++ packages/playground/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+	output: "static",
+  devToolbar: {
+    enabled: false,
+  },
 	server: {
 		headers: {
 			'Cross-Origin-Opener-Policy': 'same-origin',
@@ -11,31 +14,8 @@ export default defineConfig({
 		},
 	},
 	integrations: [
-		starlight({
-			title: 'C2W Playground',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			customCss: [
-        // Relative path to your custom CSS file
-        './src/styles/custom.css',
-      ],
-		}),
 		tailwind({
-      applyBaseStyles: false,
-    }),
+			applyBaseStyles: false,
+		}),
 	],
 });
